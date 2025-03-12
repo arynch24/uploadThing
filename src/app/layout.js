60d83f7132ce8ad2ed182021d0dad4ad918b7378
Aryan <arynch24@gmail.com>
2025-03-13 00:07:25 +0530

    Update global styles and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import "./globals.css"; // Import styles
+import "./globals.css"; // Import global styles
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen flex flex-col">
         <Header />
-        <main className="container mx-auto p-4">{children}</main>
-        <Footer /> 
+        <main className="w-full">{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
-import Image from "next/image";
-
+import Hero from "@/components/Home/Hero";
+import Section from "@/components/Home/Section";
 export default function Home() {
   return (
     <>
-      <div className="absolute left-[14px] h-[700px] w-[700px] animate-fade-in overflow-hidden">
-        {/* <Image
-          alt="File upload"
-          src="https://s40vlb3kca.ufs.sh/f/b8806777-20e8-4cb2-8769-2a09a3a43e62-1tchgi.png"
-          width={672}
-          height={700}
-          priority
-        /> */}
+      <div className="w-full">
+        <Hero />
+        <Section />
       </div>
-
-      <div className="text-4xl">Arayjhdbfjsbjfs</div>
     </>
   );
 }
