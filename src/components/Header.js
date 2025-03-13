34dfad942e67d789333f05b4312cc5d3b3a0cece
Aryan <arynch24@gmail.com>
2025-03-13 23:53:16 +0530

    Refactor Header component to include sign in and sign out functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,40 @@
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
-    return (
-        <header>
-            <div className="h-2 bg-[#E91616]"></div>
-            <div className="w-full h-24 px-[104px] py-8  flex justify-between items-center">
-                <div className="text-3xl font-bold flex items-center justify-center">
-                    <p className="text-white">upload</p>
-                    <p className="text-[#E91616]">thing</p>
-                    <p className=" flex items-center justify-center border-1 text-[9px] border-gray-400 px-0.5 py-1 rounded h-4 w-9 text-gray-400 lg:relative lg:top-1 lg:left-1">BETA</p>
-                </div>
-                <Button>Sign In</Button>
-            </div>
-        </header>
-    );
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  return (
+    <header>
+      <div className="h-2 bg-[#E91616]"></div>
+      <div className="w-full h-24 px-[104px] py-8 flex justify-between items-center">
+        <div className="text-3xl font-bold flex items-center justify-center">
+          <p className="text-white">upload</p>
+          <p className="text-[#E91616]">thing</p>
+          <p className="flex items-center justify-center border-1 border-gray-400 px-0.5 py-1 rounded h-4 w-9 text-gray-400 lg:relative lg:top-1 lg:left-1 text-[9px]">
+            BETA
+          </p>
+        </div>
+
+        {session ? (
+          <Button
+            variant="destructive"
+            onClick={() => {
+              signOut();
+              router.push("/");
+            }}
+          >
+            Sign Out
+          </Button>
+        ) : (
+          <Button variant="default" onClick={() => router.push("/signin")}>
+            Sign In
+          </Button>
+        )}
+      </div>
+    </header>
+  );
 }
