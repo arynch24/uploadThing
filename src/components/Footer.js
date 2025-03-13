802022ca482e0a9a5592ec9b1e9d35ffc2b47efd
Aryan <arynch24@gmail.com>
2025-03-13 06:40:20 +0530

    Refactor Footer component and add Button component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,26 @@
 import { FaGithub, FaDiscord, FaTwitter } from "react-icons/fa";
+import { Button } from "./ui/button";
 export default function Footer() {
     return (
         <footer className="align-bottom">
-            <div className="w-full h-24 px-[104px] py-8 text-[16px] flex justify-between items-center border-t-[0.5px] border-red-900">
+            <div className="w-full h-20 py-8 text-sm flex justify-between items-center border-t-[0.1px] border-zinc-800 px-8">
                 <p className="text-gray-400">© 2025 Ping Labs. All rights reserved.</p>
                 <div className="text-300 h-7 flex gap-10 text-gray-300">
                     <p>Terms of Service</p>
                     <p>Support</p>
                 </div>
-                <div className="text-gray-400 flex gap-8 h-9 ">
-                    <div>All Systems Operational</div>
-                    <div className="flex gap-10">
+                <div className="text-gray-400 flex items-center gap-6 h-9 ">
+                    <Button variant="ghost" size="icon"><span className="text-3xl text-green-500">•</span>
+                        All Systems Operational</Button>
+                    <div className="flex gap-6">
                         <a href="https://github.com" target="_blank">
-                            <FaGithub className="w-5 h-5  hover:text-black" />
+                            <FaGithub className="w-5 h-5  " />
                         </a>
                         <a href="https://discord.com" target="_blank">
-                            <FaDiscord className="w-5 h-5 hover:text-blue-500" />
+                            <FaDiscord className="w-5 h-5 " />
                         </a>
                         <a href="https://twitter.com" target="_blank">
-                            <FaTwitter className="w-5 h-5 hover:text-blue-400" />
+                            <FaTwitter className="w-5 h-5 " />
                         </a>
                     </div>
                 </div>
