8210c99fd9cb528d1c5300b4449ca872e8bac221
Aryan <arynch24@gmail.com>
2025-03-17 10:39:30 +0530

    Update next.config.mjs with serverActions and remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {}, // ✅ Use an empty object instead of `true`
+    serverActions: {
+      // No changes to allowedOrigins
+    },
   },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "s40vlb3kca.ufs.sh",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing-production.up.railway.app",
       },
     ],
   },
